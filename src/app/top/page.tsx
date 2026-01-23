7a5dfabc177c486f53f9f593f6d1046fbcbac9c3
chika <0119chimu@gmail.com>
2026-01-24 00:24:43 +0900

    コンポーネント化の修正、レイアウト修正

--- src/app/top/page.tsx
+++ src/app/top/page.tsx
@@ -3,39 +3,37 @@
 import { useTopPage } from "./_hooks/useTopPage";
 import { TopSummaryCard } from "./_components/TopSummaryCard";
 import { TopSubscriptionList } from "./_components/TopSubscriptionList";
+import { useSubscription } from "@/contexts/SubscriptionContext";
 
 /**
  * TOPページ：
  * 今月の合計金額・月の平均金額・今月支払いのサブスク一覧
  */
 export default function TopPage() {
+  const subscriptionData = useSubscription();
   const {
     averageMonthlyAmount,
     currentMonthTotal,
     sortedSubscriptions,
     isLoading,
-  } = useTopPage();
+  } = useTopPage(subscriptionData);
 
   return (
     <>
-      <div className="container mx-auto px-4">
-        <div className="mt-5 mb-30">
-          <div className="flex flex-col items-center">
-            <div className="sm:w-2/3 w-full">
-              {/* 今月の合計金額・月の平均金額*/}
-              <TopSummaryCard
-                total={currentMonthTotal}
-                average={averageMonthlyAmount}
-                isLoading={isLoading}
-              />
+      <div className="container mx-auto px-4 mt-5 mb-30">
+        <div className="mx-auto w-full sm:w-2/3">
+          {/* 今月の合計金額・月の平均金額*/}
+          <TopSummaryCard
+            total={currentMonthTotal}
+            average={averageMonthlyAmount}
+            isLoading={isLoading}
+          />
 
-              {/* 今月支払いのサブスク一覧 */}
-              <TopSubscriptionList
-                subscriptions={sortedSubscriptions}
-                isLoading={isLoading}
-              />
-            </div>
-          </div>
+          {/* 今月支払いのサブスク一覧 */}
+          <TopSubscriptionList
+            subscriptions={sortedSubscriptions}
+            isLoading={isLoading}
+          />
         </div>
       </div>
     </>
